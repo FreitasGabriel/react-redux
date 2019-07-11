aed53c513e74f0d06cd429afcb6976167fb5ccf5
gabriel_fs <gabriel.freitas@bemobi.com.br>
2019-07-11 11:55:04 -0300

    add reactotron on project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './config/ReactotronConfig'
 import { Provider } from 'react-redux';
 import store from './store';
 import TodoList from './TodoList'
 
+console.tron.log('Testando');
 
 const App = () => (
     <Provider store={store}>
