0c2381dcaf80abc1e2485d08af5f207e0d08502c
gabriel_fs <gabriel.freitas@bemobi.com.br>
2019-07-11 14:20:34 -0300

    adding interface

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 
-const Main = () => <h1>Main</h1>
+export default class Main extends Component {
+    
+    state = {
+        repositoryInput: '',
+    }
+    
+    render() {
+        return(
+            <Fragment>
+                <form onSubmit={this.handleAddRepository}>
+                    <input 
+                    placeholder="usuário/placeholder"
+                    value={ this.state.repositoryInput}
+                    onChange={e => this.setState({ repositoryInput: e.target.value})}/>
+                    <button type="submit">Adicionar</button>
+                </form>
 
-export default Main;
+                <ul>
+                    <li>
+                        <p>
+                            <strong>facebook/react</strong> (A declarative, efficient, and flexible JavaScript library for building user interfaces)
+                        </p>
+                        <a href="https://github.com/facebook/react"> Acessar</a>
+                    </li>
+                </ul>
+            </Fragment>
+        )
+    }
+}
